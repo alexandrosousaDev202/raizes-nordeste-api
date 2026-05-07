d01d03972df2e32de9abc48ac935b0c384817c86
alexandrosousaDev202 <alexandrosousauitec@gmail.com>
2026-05-07 14:44:35 -0300

    feat: liberacao do CORS e configuracao de rotas publicas no catalogo

--- controllers/ProdutoController.php
+++ controllers/ProdutoController.php
@@ -7,16 +7,24 @@ use yii\filters\auth\HttpBearerAuth;
 
 class ProdutoController extends ActiveController
 {
-    public $modelClass = 'app\models\Produto';
+    public $modelClass = Produto::class;
 
-    public function behaviors()
+   public function behaviors()
     {
         $behaviors = parent::behaviors();
-        
-        $behaviors['authenticator'] = [
-            'class' => HttpBearerAuth::class, 
+        $auth = $behaviors['authenticator'];
+        unset($behaviors['authenticator']);
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::class,
+            'cors' => [
+                'Origin' => ['http://localhost:5173'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+            ],
         ];
-        
+        $behaviors['authenticator'] = $auth;
+        $behaviors['authenticator']['class'] = HttpBearerAuth::class;
+        $behaviors['authenticator']['except'] = ['options', 'index', 'view'];
         return $behaviors;
     }
 }
