51e67b882b4ef98ff1775b23ac5ee0a7d471da41
alexandrosousaDev202 <alexandrosousauitec@gmail.com>
2026-05-07 18:30:42 -0300

    melhora busca de variaveis de ambiente de banco de dados

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => getenv('DB_DSN') ?: 'pgsql:host=localhost;port=5432;dbname=raizes_nordeste',
-    'username' => getenv('DB_USER') ?: 'postgres',
-    'password' => getenv('DB_PASS') ?: '3636',
+    'dsn' => $_SERVER['DB_DSN'] ?? getenv('DB_DSN') ?? 'pgsql:host=localhost;port=5432;dbname=raizes_nordeste',
+    'username' => $_SERVER['DB_USER'] ?? getenv('DB_USER') ?? 'postgres',
+    'password' => $_SERVER['DB_PASS'] ?? getenv('DB_PASS') ?? '3636',
     'charset' => 'utf8',
 ];
