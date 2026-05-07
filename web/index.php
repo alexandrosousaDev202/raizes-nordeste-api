5fa1ee67904978291bec293cbac6a3dc8431320c
alexandrosousaDev202 <alexandrosousauitec@gmail.com>
2026-05-07 17:39:54 -0300

    altera dotenv para safeLoad para evitar crash em producao sem arquivo .env

--- web/index.php
+++ web/index.php
@@ -1,6 +1,5 @@
 <?php
 
-// comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
@@ -8,7 +7,7 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
-$dotenv->load();
+$dotenv->safeLoad();
 
 $config = require __DIR__ . '/../config/web.php';
 
