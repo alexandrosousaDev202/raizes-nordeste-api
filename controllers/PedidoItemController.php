60f6140c62d507da332d2510c8be108259ab01e8
alexandrosousaDev202 <alexandrosousauitec@gmail.com>
2026-05-08 14:50:36 -0300

    fix: adiciona liberacao de cors para qualquer dominio

--- controllers/PedidoItemController.php
+++ controllers/PedidoItemController.php
@@ -3,29 +3,35 @@
 namespace app\controllers;
 
 use yii\rest\ActiveController;
-use yii\filters\auth\HttpBearerAuth;
+use yii\filters\Cors;
 use app\models\PedidoItem;
 
 class PedidoItemController extends ActiveController
 {
     public $modelClass = PedidoItem::class;
 
-     public function behaviors()
+    public function behaviors()
     {
         $behaviors = parent::behaviors();
-        $auth = $behaviors['authenticator'];
-        unset($behaviors['authenticator']);
+
+        $auth = $behaviors['authenticator'] ?? null;
+        if ($auth) {
+            unset($behaviors['authenticator']);
+        }
+
         $behaviors['corsFilter'] = [
-            'class' => \yii\filters\Cors::class,
+            'class' => Cors::class,
             'cors' => [
-                'Origin' => ['http://localhost:5173'],
-                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
                 'Access-Control-Request-Headers' => ['*'],
             ],
         ];
-        $behaviors['authenticator'] = $auth;
-        $behaviors['authenticator']['class'] = HttpBearerAuth::class;
-        $behaviors['authenticator']['except'] = ['create', 'index', 'view', 'options'];
+
+        if ($auth) {
+            $behaviors['authenticator'] = $auth;
+            $behaviors['authenticator']['except'] = ['create', 'index', 'view', 'options'];
+        }
 
         return $behaviors;
     }
